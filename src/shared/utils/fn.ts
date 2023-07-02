2521369ab882ed67bb207a6f846d29f3ea6a3d9e
XoldpandaX <rembo-87@yandex.ru>
2023-07-02 19:10:58 +0200

    feat: add fn utils

---
+++ src/shared/utils/fn.ts
@@ -0,0 +1,10 @@
+// Re-exports of lodash/ramda like libraries
+import ramdaEquals from 'ramda/src/equals';
+import ramdaCompose from 'ramda/src/compose';
+import ramdaSplit from 'ramda/src/split';
+import ramdaMap from 'ramda/src/map';
+
+export const equals = ramdaEquals;
+export const compose = ramdaCompose;
+export const split = ramdaSplit;
+export const map = ramdaMap;
